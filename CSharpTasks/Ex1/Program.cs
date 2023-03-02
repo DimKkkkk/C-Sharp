cb051fb4225ee72c16bb61d741bd05103eb67c5d
DimK <kochergin.d.a@yandex.ru>
2023-03-02 15:34:04 +0300

    Решил 1 задачу из сборника

---
+++ CSharpTasks/Ex1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача1: R=3*t^2+3*l^5+4.9
+
+Console.Clear();
+
+Console.Write("Вычислить арифметическое выражение R = 3 * t^2 + 3 * l^5 + 4.9");
+Console.WriteLine();
+
+Console.Write("Введите значение t: ");
+uint t = GetNumberFromUser();
+
+Console.Write("Введите значение l: ");
+uint l = GetNumberFromUser();
+
+double R = 3 * Math.Pow(t, 2) + 3 * Math.Pow(l, 5) + 4.9;
+Console.Write($"R = 3 * {t}^2 + 3 * {l}^5 + 4.9 = {R}");
+
+uint GetNumberFromUser()
+{
+    while (true)
+    {
+        bool isCorrect = uint.TryParse(Console.ReadLine(), out uint num);
+        if (isCorrect) return num;
+        else Console.WriteLine("Ошибка ввода!");
+    }
+}
