64cb9e1196504e99f5ecd4f32d6a92c077a0b36a
DimK <kochergin.d.a@yandex.ru>
2023-03-03 19:17:58 +0300

    Решил задачу 3 с корнем

---
+++ CSharpTasks/Ex3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача3: G = n(y+3.5)+√y)
+
+Console.Clear();
+
+Console.WriteLine("Вычислить арифметическое выражение: G = n * (y + 3.5) + √y)");
+Console.Write("Введите значение n: ");
+uint n = GetNumberFromUser();
+
+Console.Write("Введите значение y: ");
+uint y = GetNumberFromUser();
+
+double G = n * (y + 3.5) + Math.Sqrt(y);
+
+Console.Write($"G = {n} * ({y} + 3.5) + √{y} = {Math.Round(G, 2)}");
+
+uint GetNumberFromUser()
+{
+    while (true)
+    {
+        bool isCorrect = uint.TryParse(Console.ReadLine(), out uint num);
+        if (isCorrect) return num;
+        else Console.WriteLine("Ошибка ввода!");
+    }
+}
