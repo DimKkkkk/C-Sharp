36daee48957c3275ad21814e6393214498685753
DimK <kochergin.d.a@yandex.ru>
2023-03-03 11:36:34 +0300

    Решил задачу 2 арифметического выражения

---
+++ CSharpTasks/EX2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача1: K= ln(p^2+y^3)+e^p
+
+Console.Clear();
+
+Console.WriteLine("Вычислить арифметическое выражение: K = ln(p^2 + y^3) + e^p");
+
+Console.Write("Введите значение p: ");
+uint p = GetNumberFromUser();
+
+Console.Write("Введите значение y: ");
+uint y = GetNumberFromUser();
+
+double e = Math.Exp(p);
+double brace = Math.Pow(p, 2) + Math.Pow(y, 3);
+double ln = Math.Log(brace);
+
+Console.Write($"K = ln({p}^2 + {y}^3) + e^{p} = {ln + e}.");
+
+uint GetNumberFromUser()
+{
+    while (true)
+    {
+        bool isCorrect = uint.TryParse(Console.ReadLine(), out uint num);
+        if (isCorrect) return num;
+        else Console.WriteLine("Ошибка ввода!");
+    }
+}
