a97a815f9c1a3470a0a960b8c8e6a030482a78c9
DimK <kochergin.d.a@yandex.ru>
2023-03-06 16:45:07 +0300

    Решил задачу5 с cos косинусом

---
+++ CSharpTasks/Ex5/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача5: L = 1,51 * cos(x^2) + 2 * x^3
+
+Console.Clear();
+
+Console.WriteLine("Вычислить арифметическое выражение: L = 1,51 * cos(x^2) + 2 * x^3");
+
+Console.Write("Введите значение x: ");
+uint x = GetNumberFromUser();
+
+double cos = Math.Cos(x*x);
+double L = 1.51 * cos + 2 * Math.Pow(x, 3);
+
+Console.Write($"L = 1,51 * cos({x}^2) + 2 * {x}^3 = {Math.Round(L, 2)}");
+
+uint GetNumberFromUser()
+{
+    while (true)
+    {
+        bool isCorrect = uint.TryParse(Console.ReadLine(), out uint num);
+        if (isCorrect) return num;
+        else Console.WriteLine("Ошибка ввода!");
+    }
+}
