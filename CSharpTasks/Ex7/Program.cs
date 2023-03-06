69c930999aaa87e78fe2754decd57204ba167c28
DimK <kochergin.d.a@yandex.ru>
2023-03-06 17:17:31 +0300

    Решил задачу7 с модулем числа

---
+++ CSharpTasks/Ex7/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача7: N = m^2 + 2,8 * |m| + 0,55
+
+Console.Clear();
+
+Console.WriteLine("Вычислить арифметическое выражение: N = m^2 + 2,8 * |m| + 0,55");
+
+Console.Write("Введите значение m: ");
+int m = GetNumberFromUser();
+
+double N = m * m + 2.8 * Math.Abs(m) + 0.55;
+
+Console.WriteLine($"N = {m}^2 + 2,8 * |{m}| + 0,55 = {Math.Round(N, 2)}");
+
+
+int GetNumberFromUser()
+{
+    while (true)
+    {
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int num);
+        if (isCorrect) return num;
+        else Console.WriteLine("Ошибка ввода!");
+    }
+}
