23947eb1a547622a1e2fedc23f71cfb5fe01e41f
DimK <kochergin.d.a@yandex.ru>
2023-03-17 17:39:03 +0300

    Решена задача 5 ввести значения, положительные возвести в квадрат

---
+++ CSharpTasks/TaskUslovOperator/Ex5/Program.cs
@@ -0,0 +1,71 @@
+﻿/* Ввести с клавиатуры три числа, положительные возвести в квадрат, а
+отрицательные оставить без изменений.*/
+
+/*int GetNumberFromUser()
+{
+    while (true)
+    {
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int num);
+        if (isCorrect) return num;
+        else Console.WriteLine("Ошибка ввода!");
+    }
+}
+
+int CheckNum(int num)
+{
+    if (num > 0) return num * num;
+    else return num;
+
+}*/
+
+int[] GetValue(int size)
+{
+    int[] value = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        value[i] = int.Parse(Console.ReadLine() ?? "");
+    }
+    return value;
+}
+
+void PrintValue(int[] value)
+{
+    for (int i = 0; i < value.Length; i++)
+    {
+        if (i < value.Length - 1) Console.Write($"{value[i]}, ");
+        else Console.Write($"{value[i]}");
+    }
+    Console.WriteLine();
+}
+
+void CheckNumAndPow(int[] value)
+{
+    for (int i = 0; i < value.Length; i++)
+    {
+        if (value[i] > 0) value[i] = value[i] * value[i];
+
+    }
+}
+
+Console.Write("Введите количество значений: ");
+int size = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine($"Введите {size} значения: ");
+int[] value = GetValue(size);
+Console.WriteLine();
+Console.Write("Полученные значения: ");
+PrintValue(value);
+Console.WriteLine();
+Console.Write("Положительные значения возвести в квадрат, отрицательные оставить без изменений... ");
+Console.WriteLine();
+CheckNumAndPow(value);
+Console.Write("Результат: ");
+PrintValue(value);
+/*Console.Write("Введите второе значение: ");
+int B = GetNumberFromUser();
+Console.Write("Введите третье значение: ");
+int C = GetNumberFromUser();
+
+Console.WriteLine();
+Console.Write($"{CheckNum(A)}, ");
+Console.Write($"{CheckNum(B)}, ");
+Console.Write($"{CheckNum(C)}");*/
