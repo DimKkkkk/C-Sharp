58787a9cf2d0d4bbcac02c1a617f11537e599474
DimK <kochergin.d.a@yandex.ru>
2023-03-29 12:28:45 +0300

    Решил задачу 9 сравнение площади квадрата и окружности

---
+++ CSharpTasks/TaskUslovOperator/Ex9/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Определить площадь квадрата (со стороной a) и окружности (радиуса R).
+Определить, площадь какой из фигур больше. Значения a и R ввести с
+клавиатуры. Ответ вывести в виде сообщения. */
+
+Console.Clear();
+
+uint GetNumberFromUser()
+{
+    while (true)
+    {
+        bool isCorrect = uint.TryParse(Console.ReadLine(), out uint num) && (num > 0);
+        if (isCorrect) return num;
+        else Console.WriteLine("Ошибка ввода!");
+
+    }
+}
+
+void GetSquare(uint a, uint R)
+{
+    double S1, S2;
+    S1 = a * a;
+    S2 = Math.Round((Math.PI * (R * R)), 2);
+    if (S1 > S2) Console.Write($"Площадь квадрата = {S1} больше площади окружности = {S2}.");
+    else Console.Write($"Площадь окружности = {S2} больше площади квадрата = {S1}.");
+}
+
+Console.Write("Введите значение стороны квадрата: ");
+uint a = GetNumberFromUser();
+Console.WriteLine();
+Console.Write("Введите значение радиуса окружности: ");
+uint R = GetNumberFromUser();
+Console.WriteLine();
+
+GetSquare(a, R);
