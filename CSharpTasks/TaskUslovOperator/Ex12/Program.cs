033adb65b9557363da9ca4634c92ec835442380c
DimK <kochergin.d.a@yandex.ru>
2023-04-17 11:47:45 +0300

    Решена задача 12 где лежит точка на координатной плоскости

---
+++ CSharpTasks/TaskUslovOperator/Ex12/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача12. Ввести с клавиатуры координаты точки А(x,y). Определить, лежит ли
+данная точка в первой четверти. Ответ вывести в виде сообщения.*/
+
+Console.Clear();
+
+int GetCordsFromUser()
+{
+    while (true)
+    {
+        bool isCorrect= int.TryParse(Console.ReadLine(), out int num);
+        if (isCorrect) return num;
+        else Console.WriteLine("Ошибка ввода!");
+    }
+}
+
+void CheckCords(int x, int y)
+{
+    if (x > 0 && y > 0 ) Console.Write(" лежит в Первой четверти.");
+    else if (x == 0 || y == 0) Console.Write(" лежит на плоскости координат!");
+    else Console.Write(" НЕ лежит в первой четверти!");
+}
+
+Console.WriteLine("Введите координаты точки А...");
+Console.WriteLine();
+Console.Write("Введите координату Х: ");
+int x = GetCordsFromUser();
+Console.WriteLine();
+Console.Write("Введите координату Y: ");
+int y = GetCordsFromUser();
+Console.WriteLine();
+Console.Write($"Точка А ({x}, {y})");
+CheckCords(x, y);
